3ca3e7726e89d2c45f0b6e411531f0e83c51738f
Wael BEN ZID <benzid.wael@hotmail.fr>
2015-04-05 23:09:56 +0100

    Basic message queue handlers settings

--- service/settings.py
+++ service/settings.py
@@ -3,4 +3,19 @@
 MESSAGE_QUEUE = {
     'HOST': 'localhost',
     'NAME': 'messaging_service',
+    'handlers': {
+        'sms' : {
+            'class': 'service.handlers.SMSHandler',
+            'filter': {
+                'class': 'service.handlers.filters.WindowTimeFilter',
+                'extra': {
+                    'from': '18:00',
+                    'to': '06:00',
+                }
+            },
+        },
+        'email': {
+            'class': 'service.handlers.EmailHandler',
+        },
+    },
 }
