312473e87f462fd3ea1fa8d4d60f8ee6d320cbad
Wael BEN ZID <benzid.wael@hotmail.fr>
2015-04-05 23:10:22 +0100

    load_handlers utility added

--- service/handlers/utils.py
+++ service/handlers/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+from service import settings
+
 
 def load_class(name):
     """
@@ -13,4 +15,20 @@ def load_class(name):
     module = __import__('.'.join(parts[0:-1]), globals(), locals(),
                         [parts[-1]])
     return getattr(module, parts[-1])
+
+
+def load_handlers():
+    """ Load an initialized copy of message handlers. """
+    handlers = {}
+    for message_type, handler_config in settings.MESSAGE_QUEUE['handlers']:
+        handler_cls = load_class(handler_config['class'])
+        handler = handler_cls()
+        filtler_config = handler_config.get('filter', None)
+        if filtler_config:
+            filter_cls = filtler_config['class']
+            filter_kwargs = filtler_config.get('extra', {})
+            filter_instance = filter_cls(**filter_kwargs)
+            handler.filter = filter_instance
+        handlers[message_type] = handler
+    return handlers
     
